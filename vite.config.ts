9280b00d29bd9fb9ded71a8c0ebb7fa061ab5d1e
Dominik Donoch <dominik.donoch@houseofangular.io>
2024-11-29 09:09:07 +0100

    feat: setup wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@
 
 import { defineConfig } from 'vite';
 import analog from '@analogjs/platform';
+import { Nitro } from 'nitropack';
+
+const devBindingsModule = async (nitro: Nitro) => {
+  if (nitro.options.dev) {
+    nitro.options.plugins.push('./src/dev-bindings.ts');
+  }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,11 +20,13 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     analog({
-      ssr: false,
-      static: true,
-      prerender: {
-        routes: [],
+      nitro: {
+        preset: "cloudflare-pages",
+        modules: [devBindingsModule],
       },
-    }),
+      prerender: {
+        routes: ['/']
+      },
+    })
   ],
 }));
